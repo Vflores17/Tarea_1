403a4989e5247bce63e1a49ee85b0bab53fd0152
vflores <vflores2002@estudiantec.cr>
2023-03-05 09:56:53 -0600

    Se corrige el formato para poder generar de manera correcta el JavaDoc y asi poder entregar el documento con el link de GitHub y la documentacion de la tarea.

--- src/controlador/MainController.java
+++ src/controlador/MainController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
 package controlador;
 
 import java.net.URL;
@@ -15,36 +12,33 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import modelo.AgregarPersona;
-import java.lang.String;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 
 
-/**
- * FXML Controller class
+/** se declara la clase MainController para acceder a las caractisticas y metodos
+ * de modelo y posteriormente mostrarlas en la interfaz al usuario.
  *
- * @author Personal
+ * @author Vidal Flores Montero carne 2021579554 Ingenieria de Computadores.
  */
+
 public class MainController implements Initializable {
+    
+    //se definen las variables a utilizar en la interaccion con la GUI
     private int operando1;
     private int operando2;
-    private float resultado;
+    private double resultado;
     private int contador=0;
+    private String[] Lista = {"San Jose","Alajuela","Cartago","Heredia",
+        "Guanacaste","Puntarenas","Limon"};
+    
+    // se declaran los elementos graficos a utilizar en la interfaz grafica.
     @FXML
     private TextField txtNombre;
     @FXML
     private TextField txtEdad;
     @FXML
     private ChoiceBox<String> txtProvincias;
-    
-    private String[] Lista = {"San Jose","Alajuela","Cartago","Heredia","Guanacaste","Puntarenas","Limon"};
-    
-    public static ComboBox<AgregarPersona> Personas1;
-    
-    private CheckBox seleccionPersona1;
-    private CheckBox seleccionPersona4;
-    private CheckBox seleccionPersona3;
-    private CheckBox seleccionPersona2;
     @FXML
     private Button suma;
     @FXML
@@ -62,30 +56,42 @@ public class MainController implements Initializable {
     @FXML
     private ComboBox<AgregarPersona> SeleccionarPersona2=new ComboBox<>();
     
-    /**
-     * Initializes the controller class.
+    /**se inicializa el MainController, se establece aspectos iniciales de 
+     * la GUI como setear las provincias en la choiceBox para poder escogerlas.
+     * @param url se envia la ubicacion de la GUI para mostrar al usuario
+     * @param rb comunicador con cadenas claves para la comunicacon entre el 
+     * usuario y el idioma que se este utilizando
      */
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         txtProvincias.getItems().addAll(Lista);
         txtProvincias.setOnAction(this::txtProvincia);
-        //seleccionPersona1.setDisable(true);
-        //seleccionPersona2.setDisable(true);
-        //seleccionPersona3.setDisable(true);
-        //seleccionPersona4.setDisable(true);
-    }
+     }
 
-
+    /**se declara el metodo txtProvincia donde se guarda en una variable 
+     * la provincia seleccionada por el usuario para posteriormente utilizarla 
+     * para crear el objeto persona
+     *@param event es una parametro de evento de accion
+     **/
+    
     public void txtProvincia(ActionEvent event){
         String miProvincia = txtProvincias.getValue();
         if(miProvincia!=null){
-        System.out.println(miProvincia);
         }
     }
+    /**Se declara el metodo agregarPersona donde se realiza la instanciacion 
+     *de las "personas"
+     * @param event es un parametro de evento de accion
+     **/
     @FXML
-    
     private void agregarPersona(ActionEvent event) {
-        
+        /*se realizan las validaciones necesarias primero se valida 
+        que no hayan mas de 4 objetos creados de lo contrario emite una alerta.
+        Luego valida si alguno de los campos requieridos se encuentran vacio, 
+        si lo cumple emite una alerta de que falta datos por ingresar. 
+        Por ultimo, se realiza la instanciacion y se agrega a ambos comboBox 
+        como opciones*/
         String nombre = txtNombre.getText();
         String provincia = txtProvincias.getValue();
         int edad = Integer.parseInt(txtEdad.getText());
@@ -112,6 +118,10 @@ public class MainController implements Initializable {
                  SeleccionarPersona1.getItems().add(nuevaPersona);
                  SeleccionarPersona2.getItems().add(nuevaPersona);
                  System.out.println(nuevaPersona);
+                 txtNombre.setText("");
+                 txtEdad.setText("");
+                 txtProvincias.setValue(null);
+                 System.out.println(nuevaPersona);
                  }
             } catch (NumberFormatException e) {
                    Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -124,16 +134,19 @@ public class MainController implements Initializable {
     
     
 
-
+    /**se define el metodo StringNombre donde se guarda en una variable 
+     * el nombre ingresado por el usurio.
+     *@param event parametro de accion de evento en la GUI.
+     **/
     @FXML
     private void stringNombre(ActionEvent event) {
         String nombre = txtNombre.getText();
     }
 
-    @FXML
-    private void intEdad(ActionEvent event) {
-    }
-
+    /**se define el metodo donde se envia los operando a utilizar 
+     * y se realiza la operacion aritmetica SUMA.
+     * @param event parametro de accion de evento en la GUI.
+     **/
     @FXML
     private void SUMAR(ActionEvent event) {
         operando1=SeleccionarPersona1.getValue().getEdad();
@@ -142,7 +155,11 @@ public class MainController implements Initializable {
         txtResultado.setText(resultado+" ");
         System.out.println(resultado);
     }
-
+    
+    /**se define el metodo MULTIPLICAR donde se envia los operando a utilizar 
+     * y se realiza la operacion aritmetica MULTIPLICACION.
+     * @param event parametro de accion de evento en la GUI.
+     **/
     @FXML
     private void MULTIPLICAR(ActionEvent event) {
         operando1=SeleccionarPersona1.getValue().getEdad();
@@ -152,6 +169,10 @@ public class MainController implements Initializable {
         System.out.println(resultado);
     }
 
+    /**se define elmetodo DIVIDIR donde se envia los operandos a utilizar 
+     * y se realiza la operacion aritmetica DIVISION.
+     * @param event parametro de accion de evento en la GUI.
+     **/
     @FXML
     private void DIVIDIR(ActionEvent event) {
         operando1=SeleccionarPersona1.getValue().getEdad();
@@ -161,6 +182,10 @@ public class MainController implements Initializable {
         System.out.println(resultado);
     }
 
+    /**se define el metodo RESTAR donde se envia los operandos a utilizar 
+     * y se realiza la operacion aritmetica RESTA.
+     * @param event parametro de accion de evento en la GUI.
+     **/
     @FXML
     private void RESTAR(ActionEvent event) {
         operando1=SeleccionarPersona1.getValue().getEdad();
@@ -170,7 +195,5 @@ public class MainController implements Initializable {
         System.out.println(resultado);
     }
 
-    
-    
-    }
+}
     
